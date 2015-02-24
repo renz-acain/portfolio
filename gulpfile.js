c1880b7379ff20b2afe00d0a3e4e234a6b35d7e0
renz-acain <renz.acain@gmail.com>
2015-02-24 13:35:04 +0000

    Add browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
 var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass');
+    sass = require('gulp-ruby-sass'),
+    browserSync = require('browser-sync');
+
+gulp.task('browser-sync', function () {
+    browserSync.init(null, {
+        proxy: "renzluck-acain.dev",
+        startPath: "/index.html",
+        browser: []
+    });
+});
 
 gulp.task('sass', function() {
     sass('scss/')
         .on('error', function (err) {
             console.error('Error!', err.message);
         })
-        .pipe(gulp.dest('./'));
+        .pipe(gulp.dest('./'))
+        .pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('watch', function() {
